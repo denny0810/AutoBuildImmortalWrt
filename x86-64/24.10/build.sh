a4c9dff469a912adcd7ed799390df5f292bbf185
denny0810 <59128785+denny0810@users.noreply.github.com>
2025-04-12 20:23:11 +0800

    Update build.sh
    remove vpn

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -46,8 +46,6 @@ PACKAGES="$PACKAGES luci-app-openclash"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
-# 增加IPSec服务器
-PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
